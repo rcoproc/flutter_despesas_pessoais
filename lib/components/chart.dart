bdba02e8ab15611fea9041d0f92ddd4fd6b74ab3
Ricardo César de Oliveira <rioliveira@bionexo.com>
2020-03-22 02:48:40 -0300

    Change ListView to ListTile

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -33,20 +33,32 @@ class Chart extends StatelessWidget {
     });
   }
 
+  double get _weekTotalValue {
+    return groupedTransactions.fold(0.0, (sum,tr) {
+      return sum + tr['value'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    groupedTransactions;
     return Card(
         elevation: 6,
         margin: EdgeInsets.all(20),
-        child: Row(
-          children: groupedTransactions.map((tr) {
-            return ChartBar(
-              label: tr['day'],
-              value: tr['value'],
-              percentage: 0.5,
-            );
-          }).toList(),
+        child: Padding(
+          padding: const EdgeInsets.all(11),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: groupedTransactions.map((tr) {
+              return Flexible(
+                fit: FlexFit.tight,
+                child: ChartBar(
+                  label: tr['day'],
+                  value: tr['value'],
+                  percentage: (tr['value'] as double) / _weekTotalValue,
+                ),
+              );
+            }).toList(),
+          ),
         ));
   }
 }
